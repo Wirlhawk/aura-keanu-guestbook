8ec1baa78bc2fc292190a8f9e54fc58968cb9639
wirlhawk <rayhantogarlumbantobing@gmail.com>
2025-04-21 13:56:32 +0700

    anjai

--- src/app/scan/page.tsx
+++ src/app/scan/page.tsx
@@ -4,137 +4,176 @@ import React, { useState } from "react";
 import Image from "next/image";
 import { Guest } from "@/types/supabase";
 import { createClient } from "@/utils/supabase/client";
-import toast, { Toaster } from "react-hot-toast";
-import { motion } from "motion/react";
+import toast from "react-hot-toast";
+import { AnimatePresence, motion } from "motion/react";
 
 const ScanPage = () => {
-    const [guest, setGuest] = useState<Guest | null>(null);
+  const [guest, setGuest] = useState<Guest | null>(null);
 
-    const onScan = async (result: IDetectedBarcode[]) => {
-        if (result.length === 0) return;
+  const onScan = async (result: IDetectedBarcode[]) => {
+    if (result.length === 0) return;
 
-        const guestId = result[0].rawValue;
-        const supabase = createClient();
+    const guestId = result[0].rawValue;
+    const supabase = createClient();
 
-        try {
-            const { data, error, count } = await supabase
-                .from("guests")
-                .select("*")
-                .eq("id", guestId)
-                .single();
+    try {
+      const { data, error, count } = await supabase
+        .from("guests")
+        .select("*")
+        .eq("id", guestId)
+        .single();
 
-            if (error) {
-                console.error("Error fetching guest:", error.message);
-                toast.error("Terjadi Kesalahan, silahkan coba lagi");
-                setGuest(null);
-                return;
-            }
+      if (error) {
+        console.error("Error fetching guest:", error.message);
+        toast.error("Terjadi Kesalahan, silahkan coba lagi");
+        setGuest(null);
+        return;
+      }
 
-            if (!data || count === 0) {
-                console.error("Error guests not found");
-                toast.error("Terjadi Kesalahan, silahkan coba lagi");
+      if (!data || count === 0) {
+        console.error("Error guests not found");
+        toast.error("Tamu tidak ditemukan");
 
-                return;
-            }
+        return;
+      }
 
-            toast.success("Sukses");
-            setGuest(data);
-        } catch (err) {
-            console.error("Unexpected error:", err);
-            toast.error("Terjadi Kesalahan, silahkan coba lagi");
-            setGuest(null);
-        }
-    };
+      toast.success("Sukses");
+      setGuest(data);
+    } catch (err) {
+      console.error("Unexpected error:", err);
+      toast.error("Terjadi Kesalahan, silahkan coba lagi");
+      setGuest(null);
+    }
+  };
 
-    return (
-        <div className="bg-white bg-[url('/assets/background/5.png')] h-[100lvh] w-full flex flex-row py-10 px-12 gap-15 font-forum relative">
-            <div className="w-1/2 flex flex-col items-center text-center">
-                <div className=" p-5 rounded">
-                    <Scanner
-                        classNames={{}}
-                        onScan={onScan}
-                        allowMultiple={true}
-                        constraints={{ facingMode: "", aspectRatio: 1 }}
-                        scanDelay={1000}
-                        components={{ finder: false }}
-                        styles={{
-                            container: { height: "fit-content" },
-                            video: {
-                                height: "fit-content",
-                                borderRadius: "20px",
-                            },
-                        }}
-                    />
-                </div>
+  return (
+    <div className="bg-white bg-[url('/assets/background/5.png')] h-[100lvh] w-full flex flex-row py-10 px-12 gap-15 font-forum relative font-forum">
+      <ScannerSection onScan={onScan} />
+      <WelcomeSection guest={guest} />
 
-                <div className="w-full flex flex-col px-10 gap-2">
-                    <h1 className="text-3xl text-primary">Scan Qr</h1>
-                    <p className="text-secondary">
-                        untuk mengisi buku tamu dan turut merayakan momen
-                        bahagia ini 💖
-                    </p>
-                </div>
-            </div>
-
-            <div className="w-1/2 font-forum items-center pt-5 text-center flex flex-col justify-between ">
-                <div className="">
-                    <h1 className="text-3xl text-primary">Welcome</h1>
-                    <p className="text-secondary">
-                        To The Wedding of Aura & Keanu
-                    </p>
-                </div>
-                {guest ? (
-                    <motion.div
-                        className="space-y-4"
-                        initial={{ opacity: 0, y: 20 }}
-                        animate={{ opacity: 1, y: 0 }}
-                        transition={{ duration: 0.5 }}
-                    >
-                        <h2 className="text-lg text-secondary">
-                            Yth. Bpk/Ibu/Sdr/i
-                        </h2>
-                        <h1 className="text-4xl text-primary font-bold">
-                            {guest.name}
-                        </h1>
-                        <p className="mb-5 text-secondary"> ID: {guest.id}</p>
-                        <p className="px-10 text-secondary">
-                            Thank you for coming and sharing this special moment
-                            with us 💖
-                        </p>
-                    </motion.div>
-                ) : (
-                    <h1 className="">
-                        Scan the QR code to check in
-                        <br /> and leave a message
-                    </h1>
-                )}
-
-                <Image
-                    src={"/assets/header.png"}
-                    width={250}
-                    height={250}
-                    className="max-w-40"
-                    alt="aura & keanu"
-                />
-            </div>
-
-            <Image
-                src="/assets/flower/7.png"
-                width={300}
-                height={300}
-                alt=""
-                className="fixed -bottom-3 right-0 translate-x-1/2 w-32 "
-            />
-            <Image
-                src="/assets/flower/18.png"
-                width={300}
-                height={300}
-                alt=""
-                className="fixed -bottom-3 left-0 -translate-x-1/2 w-32 "
-            />
-            <Toaster />
-        </div>
-    );
+      <Image
+        src="/assets/flower/7.png"
+        width={300}
+        height={300}
+        alt=""
+        className="fixed -bottom-3 right-0 translate-x-1/2 w-32 "
+      />
+      <Image
+        src="/assets/flower/18.png"
+        width={300}
+        height={300}
+        alt=""
+        className="fixed -bottom-3 left-0 -translate-x-1/2 w-32 "
+      />
+    </div>
+  );
 };
+
+function ScannerSection({
+  onScan,
+}: {
+  onScan: (result: IDetectedBarcode[]) => void;
+}) {
+  return (
+    <div className="w-1/2 flex flex-col items-center text-center my-auto">
+      <div className=" p-5 rounded">
+        <Scanner
+          onScan={(result) =>
+            toast.promise(async () => await onScan(result), {
+              loading: "Scanning...",
+            })
+          }
+          allowMultiple={true}
+          constraints={{ facingMode: "", aspectRatio: 1 }}
+          scanDelay={3000}
+          components={{ finder: false }}
+          styles={{
+            container: { height: "fit-content" },
+            video: {
+              height: "fit-content",
+              borderRadius: "20px",
+            },
+          }}
+        />
+      </div>
+
+      <div className="w-full flex flex-col px-10 gap-2">
+        <h1 className="text-3xl text-primary">Scan Qr</h1>
+        <p className="text-secondary">
+          untuk mengisi buku tamu dan turut merayakan momen bahagia ini 💖
+        </p>
+      </div>
+    </div>
+  );
+}
+
+function WelcomeSection({ guest }: { guest: Guest | null }) {
+  return (
+    <div className="w-1/2 font-forum items-center pt-5 text-center flex flex-col justify-between ">
+      <div className="">
+        <h1 className="text-3xl text-primary">Welcome</h1>
+        <p className="text-secondary">To The Wedding of Aura & Keanu</p>
+      </div>
+      {guest ? (
+        <GuestCard guest={guest} />
+      ) : (
+        <h1 className="text-secondary text-xl">
+          Scan the QR code to check in
+          <br /> and leave a message
+        </h1>
+      )}
+
+      <Image
+        src={"/assets/header.png"}
+        width={250}
+        height={250}
+        className="max-w-40"
+        alt="aura & keanu"
+      />
+    </div>
+  );
+}
+
+function GuestCard({ guest }: { guest: Guest }) {
+  return (
+    <AnimatePresence>
+      <motion.div
+        key={guest.id}
+        className="border-secondary border flex p-1 relative"
+        initial={{ y: 150, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+        transition={{ duration: 1 }}
+      >
+        <div className="bg-secondary flex flex-col items-center w-full py-5 px-4 gap-3">
+          <h2 className="text-lg text-white">Yth. Bpk/Ibu/Sdr/i</h2>
+          <div>
+            <h1 className="text-2xl  font-bold text-yellow-500">
+              {guest.name}
+            </h1>
+            <p className="tracking-widest text-yellow-500 text-md">VIP GUEST</p>
+          </div>
+          <p className="px-10 text-white text-xs">
+            Thank you for coming and sharing this special moment with us 💖
+          </p>
+        </div>
+
+        <Image
+          width={200}
+          height={200}
+          src="/assets/flower/6.png"
+          alt=""
+          className="absolute w-20 left-1/2 -translate-x-1/2 top-0 -translate-y-3/4"
+        />
+        <Image
+          width={200}
+          height={200}
+          src="/assets/flower/8.png"
+          alt=""
+          className="absolute w-14 left-1/2 -translate-x-1/2 bottom-0 translate-y-3/4 z-20"
+        />
+      </motion.div>
+    </AnimatePresence>
+  );
+}
 
 export default ScanPage;
